b58a2bbe4265dbbae729de1f994ddea378ff672d
Tristan <tristanchay123@gmail.com>
2023-12-12 11:12:06 +0800

    Initial Commit

---
+++ Hyundone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Hyundone
+//
+//  Created by Tristan Chay on 12/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hyundone/HyundoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  HyundoneApp.swift
+//  Hyundone
+//
+//  Created by Tristan Chay on 12/12/23.
+//
+
+import SwiftUI
+
+@main
+struct HyundoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
